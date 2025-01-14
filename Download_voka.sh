ade93c83113f2da23b3cf6de965a8d668b962af4
Ers <testerfortest37@gmail.com>
2025-01-14 08:35:11 +0300

    update voka v22064

--- Download_voka.sh
+++ Download_voka.sh
@@ -1,4 +1,4 @@
 #!/data/data/com.termux/files/usr/bin/sh
-wget https://voka.tv/apps/Voka-prod-androidTv-app-default-release-21984.apk;
+wget https://voka.tv/apps/TvVoka-api21-prod-leanback-app-default-release-v22064.apk;
 wget https://voka.tv/apps/TvVoka-21805.apk;
 find . -maxdepth 1 -mindepth 1 -type f -name "*apk.*"    -exec rm -rf {} \;
