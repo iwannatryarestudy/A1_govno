942d87d5b65db333bd974bb89b77d8a6556080a9
Ers <testerfortest37@gmail.com>
2025-07-11 14:51:15 +0300

    done100001

--- stable_voka_and_install.sh
+++ stable_voka_and_install.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/sh
 read -p "Введите 2 последних октета " var1;
 adb connect 192.168.$var1:5555 & sleep 5;
-VOka="Voka-prod-androidTv-app-default-release-21984.apk"
+VOka="TvVoka-api21-prod-leanback-app-default-release-v22234.apk"
 adb shell pm uninstall  --user 0 com.spbtv.velcom && echo "del" & sleep 5;
 adb install  $VOka && echo "no downgrade" || adb install -d $VOka && echo "install downgrade 21984_version";
 adb disconnect 1>/dev/null;
