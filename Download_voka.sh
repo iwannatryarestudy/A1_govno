5ddaa9be18ca8a4671f11a16e99315a67811ddcd
Ers <testerfortest37@gmail.com>
2025-07-11 12:41:08 +0300

    pull

--- Download_voka.sh
+++ Download_voka.sh
@@ -1,5 +1,4 @@
 #!/data/data/com.termux/files/usr/bin/sh
 wget https://voka.tv/apps/TvVoka-api21-prod-leanback-app-default-release-v22064.apk;
-wget https://voka.tv/apps/TvVoka-21805.apk;
 find . -maxdepth 1 -mindepth 1 -type f -name "*apk.*" ! -name "TvVoka-21805.apk" -exec rm -f {} \;
 
--- update_script.sh
+++ update_script.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/sh
 #
-rm -rf A1_govno;
-git clone https://github.com/iwannatryarestudy/A1_govno.git;
-unzip -o A1_govno/stick.zip;
+#rm -rf A1_govno;
+git pull
+#unzip -o A1_govno/stick.zip;
 sh Download_voka.sh
