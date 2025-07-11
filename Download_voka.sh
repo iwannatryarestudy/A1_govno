3703932426ad34da9998fc26ef80bae5daa00d27
Ers <testerfortest37@gmail.com>
2025-07-11 13:17:39 +0300

    Download.sh

--- Download_voka.sh
+++ Download_voka.sh
@@ -1,4 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/sh
 wget https://voka.tv/apps/TvVoka-api21-prod-leanback-app-default-release-v22064.apk;
+wget https://voka.tv/apps/TvVoka-21805.apk;
 find . -maxdepth 1 -mindepth 1 -type f -name "*apk.*" ! -name "TvVoka-21805.apk" -exec rm -f {} \;
 
